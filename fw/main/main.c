fd8305a69249a0059c424422ee8bad794b352cf0
Ayu <claire@ayu.land>
2024-11-19 17:22:18 +0800

    fw: HTTP requests

--- fw/main/main.c
+++ fw/main/main.c
@@ -14,7 +14,10 @@
 #include "esp_wifi.h"
 #include "nvs_flash.h"
 
+#include "lwip/dns.h"
 #include "lwip/err.h"
+#include "lwip/netdb.h"
+#include "lwip/sockets.h"
 #include "lwip/sys.h"
 
 #define EXAMPLE_ESP_WIFI_SSID      "Future Lab-6F"
@@ -34,7 +37,8 @@ static EventGroupHandle_t s_wifi_event_group;
 #define WIFI_CONNECTED_BIT BIT0
 #define WIFI_FAIL_BIT      BIT1
 
-static const char *TAG = "wifi station";
+#undef TAG
+#define TAG "Wi-Fi STA"
 
 static int s_retry_num = 0;
 
@@ -128,6 +132,103 @@ void wifi_init_sta(void)
   }
 }
 
+#define WEB_SERVER "example.com"
+#define WEB_PORT "80"
+#define WEB_PATH "/"
+
+#undef TAG
+#define TAG "HTTP client"
+
+static const char *REQUEST = "GET " WEB_PATH " HTTP/1.0\r\n"
+    "Host: "WEB_SERVER":"WEB_PORT"\r\n"
+    "User-Agent: esp-idf/1.0 esp32\r\n"
+    "\r\n";
+
+static void http_get_task(void *pvParameters)
+{
+  const struct addrinfo hints = {
+    .ai_family = AF_INET,
+    .ai_socktype = SOCK_STREAM,
+  };
+  struct addrinfo *res;
+  struct in_addr *addr;
+  int s, r;
+  char recv_buf[64];
+
+  while (1) {
+    int err = getaddrinfo(WEB_SERVER, WEB_PORT, &hints, &res);
+
+    if (err != 0 || res == NULL) {
+      ESP_LOGE(TAG, "DNS lookup failed err=%d res=%p", err, res);
+      vTaskDelay(1000 / portTICK_PERIOD_MS);
+      continue;
+    }
+
+    /* Code to print the resolved IP.
+
+       Note: inet_ntoa is non-reentrant, look at ipaddr_ntoa_r for "real" code */
+    addr = &((struct sockaddr_in *)res->ai_addr)->sin_addr;
+    ESP_LOGI(TAG, "DNS lookup succeeded. IP=%s", inet_ntoa(*addr));
+
+    s = socket(res->ai_family, res->ai_socktype, 0);
+    if (s < 0) {
+      ESP_LOGE(TAG, "... Failed to allocate socket.");
+      freeaddrinfo(res);
+      vTaskDelay(1000 / portTICK_PERIOD_MS);
+      continue;
+    }
+    ESP_LOGI(TAG, "... allocated socket");
+
+    if (connect(s, res->ai_addr, res->ai_addrlen) != 0) {
+      ESP_LOGE(TAG, "... socket connect failed errno=%d", errno);
+      close(s);
+      freeaddrinfo(res);
+      vTaskDelay(4000 / portTICK_PERIOD_MS);
+      continue;
+    }
+
+    ESP_LOGI(TAG, "... connected");
+    freeaddrinfo(res);
+
+    if (write(s, REQUEST, strlen(REQUEST)) < 0) {
+      ESP_LOGE(TAG, "... socket send failed");
+      close(s);
+      vTaskDelay(4000 / portTICK_PERIOD_MS);
+      continue;
+    }
+    ESP_LOGI(TAG, "... socket send success");
+
+    struct timeval receiving_timeout;
+    receiving_timeout.tv_sec = 5;
+    receiving_timeout.tv_usec = 0;
+    if (setsockopt(s, SOL_SOCKET, SO_RCVTIMEO, &receiving_timeout,
+                   sizeof(receiving_timeout)) < 0) {
+      ESP_LOGE(TAG, "... failed to set socket receiving timeout");
+      close(s);
+      vTaskDelay(4000 / portTICK_PERIOD_MS);
+      continue;
+    }
+    ESP_LOGI(TAG, "... set socket receiving timeout success");
+
+    /* Read HTTP response */
+    do {
+      bzero(recv_buf, sizeof(recv_buf));
+      r = read(s, recv_buf, sizeof(recv_buf)-1);
+      for (int i = 0; i < r; i++) {
+        putchar(recv_buf[i]);
+      }
+    } while (r > 0);
+
+    ESP_LOGI(TAG, "... done reading from socket. Last read return=%d errno=%d.", r, errno);
+    close(s);
+    for (int countdown = 10; countdown >= 0; countdown--) {
+      ESP_LOGI(TAG, "%d... ", countdown);
+      vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+    ESP_LOGI(TAG, "Starting again!");
+  }
+}
+
 void app_main(void)
 {
   printf("Hello world!\n");
@@ -173,6 +274,8 @@ void app_main(void)
   This is esp32s3 chip with 2 CPU core(s), WiFi/BLE, silicon revision v0.2, 8MB external flash
   Minimum free heap size: 389836 bytes
 */
+
+  xTaskCreate(&http_get_task, "http_get_task", 4096, NULL, 5, NULL);
 
   while (1) {
     printf("Tick\n");
