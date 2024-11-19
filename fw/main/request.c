0dc92c90eb7a6b4f17c2008cd06de526ab22c3c6
Ayu <claire@ayu.land>
2024-11-19 21:53:48 +0800

    fw: Simplify HTTP client event handling

--- fw/main/request.c
+++ fw/main/request.c
@@ -5,18 +5,15 @@
 #include "esp_tls.h"
 #include "esp_crt_bundle.h"
 
-#define MAX_HTTP_OUTPUT_BUFFER 2048
+#define MAX_HTTP_OUTPUT_BUFFER 4096
 #define TAG "HTTP client"
 
-#define MIN(_a, _b) ((_a) < (_b) ? (_a) : (_b))
-
-// examples/protocols/esp_http_client/main/esp_http_client_example.c
+#define min(_a, _b) ((_a) < (_b) ? (_a) : (_b))
 
 static esp_err_t http_event_handler(esp_http_client_event_t *evt)
 {
-  static char *output_buffer;  // Buffer to store response of http request from event handler
-  static int output_len;       // Stores number of bytes read
-  switch(evt->event_id) {
+  static int output_ptr;
+  switch (evt->event_id) {
     case HTTP_EVENT_ERROR:
       ESP_LOGD(TAG, "HTTP_EVENT_ERROR");
       break;
@@ -31,53 +28,15 @@ static esp_err_t http_event_handler(esp_http_client_event_t *evt)
       break;
     case HTTP_EVENT_ON_DATA:
       ESP_LOGD(TAG, "HTTP_EVENT_ON_DATA, len=%d", evt->data_len);
-      // Clean the buffer in case of a new request
-      if (output_len == 0 && evt->user_data) {
-        // we are just starting to copy the output data into the use
-        memset(evt->user_data, 0, MAX_HTTP_OUTPUT_BUFFER);
-      }
-      /*
-       *  Check for chunked encoding is added as the URL for chunked encoding used in this example returns binary data.
-       *  However, event handler can also be used in case chunked encoding is used.
-       */
       if (!esp_http_client_is_chunked_response(evt->client)) {
-        // If user_data buffer is configured, copy the response into the buffer
-        int copy_len = 0;
-        if (evt->user_data) {
-          // The last byte in evt->user_data is kept for the NULL character in case of out-of-bound access.
-          copy_len = MIN(evt->data_len, (MAX_HTTP_OUTPUT_BUFFER - output_len));
-          if (copy_len) {
-            memcpy(evt->user_data + output_len, evt->data, copy_len);
-          }
-        } else {
-          int content_len = esp_http_client_get_content_length(evt->client);
-          if (output_buffer == NULL) {
-            // We initialize output_buffer with 0 because it is used by strlen() and similar functions therefore should be null terminated.
-            output_buffer = (char *) calloc(content_len + 1, sizeof(char));
-            output_len = 0;
-            if (output_buffer == NULL) {
-              ESP_LOGE(TAG, "Failed to allocate memory for output buffer");
-              return ESP_FAIL;
-            }
-          }
-          copy_len = MIN(evt->data_len, (content_len - output_len));
-          if (copy_len) {
-            memcpy(output_buffer + output_len, evt->data, copy_len);
-          }
-        }
-        output_len += copy_len;
+        int copy_len = min(evt->data_len, MAX_HTTP_OUTPUT_BUFFER - output_ptr);
+        memcpy(evt->user_data + output_ptr, evt->data, copy_len);
+        output_ptr += copy_len;
       }
-
       break;
     case HTTP_EVENT_ON_FINISH:
       ESP_LOGD(TAG, "HTTP_EVENT_ON_FINISH");
-      if (output_buffer != NULL) {
-        // Response is accumulated in output_buffer. Uncomment the below line to print the accumulated response
-        // ESP_LOG_BUFFER_HEX(TAG, output_buffer, output_len);
-        free(output_buffer);
-        output_buffer = NULL;
-      }
-      output_len = 0;
+      output_ptr = 0;
       break;
     case HTTP_EVENT_DISCONNECTED:
       ESP_LOGI(TAG, "HTTP_EVENT_DISCONNECTED");
@@ -87,17 +46,11 @@ static esp_err_t http_event_handler(esp_http_client_event_t *evt)
         ESP_LOGI(TAG, "Last esp error code: 0x%x", err);
         ESP_LOGI(TAG, "Last mbedtls failure: 0x%x", mbedtls_err);
       }
-      if (output_buffer != NULL) {
-        free(output_buffer);
-        output_buffer = NULL;
-      }
-      output_len = 0;
+      output_ptr = 0;
       break;
     case HTTP_EVENT_REDIRECT:
-      ESP_LOGD(TAG, "HTTP_EVENT_REDIRECT");
-      esp_http_client_set_header(evt->client, "From", "user@example.com");
-      esp_http_client_set_header(evt->client, "Accept", "text/html");
-      esp_http_client_set_redirection(evt->client);
+      // `HTTP_EVENT_REDIRECT` will not be dispatched if `disable_auto_redirect` is off
+      // i.e. redirects automatically (see `esp-idf/components/esp_http_client/esp_http_client.c`)
       break;
   }
   return ESP_OK;
@@ -105,7 +58,7 @@ static esp_err_t http_event_handler(esp_http_client_event_t *evt)
 
 void http_get_task(void *_unused)
 {
-  static char local_response_buffer[MAX_HTTP_OUTPUT_BUFFER + 1] = {0};
+  static char local_response_buffer[MAX_HTTP_OUTPUT_BUFFER];
 
   esp_err_t err;
   esp_http_client_handle_t client = esp_http_client_init(&(esp_http_client_config_t){
@@ -118,18 +71,21 @@ void http_get_task(void *_unused)
   });
 
   while (1) {
-    esp_http_client_set_url(client, "https://ayu.land/"),
+    static bool parity = 0;
+    esp_http_client_set_url(client,
+      (parity ^= 1) ? "https://www.howsmyssl.com/a/check" : "http://ayu.land"),
     esp_http_client_set_method(client, HTTP_METHOD_GET);
     err = esp_http_client_perform(client);
 
     if (err == ESP_OK) {
+      int64_t len = esp_http_client_get_content_length(client);
       ESP_LOGI(TAG, "HTTP GET Status = %d, content_length = %"PRId64,
               esp_http_client_get_status_code(client),
-              esp_http_client_get_content_length(client));
+              len);
+      ESP_LOG_BUFFER_HEX(TAG, local_response_buffer, min(len, MAX_HTTP_OUTPUT_BUFFER));
     } else {
       ESP_LOGE(TAG, "HTTP GET request failed: %s", esp_err_to_name(err));
     }
-    ESP_LOG_BUFFER_HEX(TAG, local_response_buffer, strlen(local_response_buffer));
     vTaskDelay(5000 / portTICK_PERIOD_MS);
   }
 
