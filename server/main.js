3b1b47fe8cd1c6f5c2139938cebf8054943575f1
Ayu <claire@ayu.land>
2024-11-25 00:36:16 +0800

    server: Integrate Google's API

--- server/main.js
+++ server/main.js
@@ -1,14 +1,18 @@
 const key = Deno.env.get('API_KEY') || prompt('API key:')
 
+const API_ENDPOINT = 'google'
+
+{
 const API_ENDPOINT = 'https://api.lingyiwanwu.com/v1/chat/completions'
 const API_MODEL = 'yi-lightning'
+}
 
 {
 const API_ENDPOINT = 'https://spark-api-open.xf-yun.com/v1/chat/completions'
 const API_MODEL = 'generalv3.5'
 }
 
-const requestLLM = async (messages) => {
+const requestLLMOpenAI = async (step, messages) => {
   const req = await fetch(
     API_ENDPOINT,
     {
@@ -28,7 +32,7 @@ const requestLLM = async (messages) => {
   return resp
 }
 
-const responseText = (r) => {
+const responseTextOpenAI = (r) => {
   if (!(r.choices instanceof Array) ||
       r.choices.length !== 1 ||
       typeof r.choices[0] !== 'object' ||
@@ -39,26 +43,66 @@ const responseText = (r) => {
   return r.choices[0].message.content
 }
 
+const requestLLMGoogle = async (step, messages) => {
+  // https://ai.google.dev/api/generate-content#v1beta.models.generateContent
+  const API_MODEL = (step === 1 ? 'gemini-1.5-flash' : 'gemini-1.5-pro')
+  const req = await fetch(
+    'https://generativelanguage.googleapis.com/v1beta/models/' + API_MODEL + ':generateContent?key=' + key,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        systemInstruction: { parts: [{ text:
+          messages.filter(({ role }) => role === 'system')
+                  .map(({ content }) => content).join('\n'),
+        }] },
+        contents:
+          messages.filter(({ role }) => role === 'user')
+                  .map(({ content }) => ({
+                    role: 'user',
+                    parts: [{ text: content }],
+                  })),
+      }),
+    }
+  )
+  const resp = await req.json()
+  return resp
+}
+
+const responseTextGoogle = (r) => {
+  const { candidates } = r
+  const [ candidate ] = candidates
+  const { content } = candidate
+  const { parts } = content
+  if (!(parts instanceof Array)) throw new Error('Incorrect format!')
+  return parts.map(({ text }) => text || '').join('')
+}
+
+const requestLLM = (API_ENDPOINT === 'google' ? requestLLMGoogle : requestLLMOpenAI);
+const responseText = (API_ENDPOINT === 'google' ? responseTextGoogle : responseTextOpenAI);
+
 const run = async (pedestrianMessage) => {
   const t0 = Date.now()
 
-  const r1 = await requestLLM([
+  const r1 = await requestLLM(1, [
     { role: 'system', content: '你是雪地里一只由雪堆成的小鸭子。小鸭不会说话，但它的身体里有一个彩色小灯，小鸭以灯光的颜色回应行人；乘着自由的想象，它觉得变化的色彩也许能承载世间的一切。\n\n请你先展开想象，描述小鸭的回应，再在结尾用简短的几句话概括具体效果（例如，“先缓缓亮起……，再逐渐过渡到……”）。' },
     { role: 'user', content: pedestrianMessage },
   ])
-  console.log(r1, Date.now() - t0)
+  console.log(Deno.inspect(r1, { depth: 99 }), Date.now() - t0)
   const lightFullText = responseText(r1)
   const lightDescription = (lightFullText.trim().match(/\n(.+)$/) || [])[1]
   if (!lightDescription) throw new Error('Malformed response')
 
-  const r2 = await requestLLM([
+  const r2 = await requestLLM(2, [
     { role: 'system', content: '你是一名交互动画工程师，正在为一盏小灯编写动画效果。你了解色彩与动画原理，认真对待细节，采用合适的混色、过渡、缓动函数等方式来实现赏心悦目的动态色彩。' },
     { role: 'user', content:
       `“${lightDescription}”\n\n` +
       '灯珠的颜色由一个 JavaScript 函数 `frame_light()` 计算。这个函数每隔 100 ms 被调用一次，它返回三个 0~255 范围内的数值，依次表示当前时刻灯光的 R、G、B 分量。需要保存的数据可置于全局变量。只需给出你编写的 `frame_light()` 函数即可，定时调用的部分不必编写。不必给出过多的解释，但请多加注意时间与速率方面，避免过快或过慢。如果动画是循环的，请在结尾时多停留一会儿。'
     }
   ])
-  console.log(r2, Date.now() - t0)
+  console.log(Deno.inspect(r2, { depth: 99 }), Date.now() - t0)
   const responseWithCode = responseText(r2)
 
   const code = (responseWithCode.match(/^```[^\n]+\n(.*?)(?<=\n)```\s*$/sm) || [])[1]
