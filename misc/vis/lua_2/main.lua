4d70e4e94f51824918bbd818166fe77d09d8f4c2
Ayu <claire@ayu.land>
2024-12-01 19:54:59 +0800

    misc: Move updated Lua visualisation to LOVE

--- misc/vis/lua_2/main.lua
+++ misc/vis/lua_2/main.lua
@@ -1,22 +1,64 @@
+local program_t = 0
+local program_r, program_g, program_b = 0, 0, 0
+
+local delay = function (t)
+  program_t = program_t + t / 1000
+end
+
+local output = function (r, g, b)
+  if r == nil then
+    return program_r, program_g, program_b
+  end
+  program_r, program_g, program_b = r, g, b
+  coroutine.yield(program_t, r, g, b)
+end
+
+local load_duck = function (s)
+  fenv = {
+    math = math,
+    os = { time = os.time },
+    delay = delay,
+    output = output,
+  }
+  local f, e = load(s .. '\nreturn light', 'duck')
+  if e then print(e) end
+  local f = setfenv(f, fenv)
+  return f()
+end
+
+local duck_fn = load_duck(io.popen('pbpaste'):read('a'))
+local co_duck = coroutine.create(function ()
+  while true do duck_fn() end
+end)
+
 local T = 0
-local function delay(t)
-  T = T + t / 1000
-  if T >= 30 then os.exit() end
-end
-local R, G, B = 0, 0, 0
-local function output(r, g, b)
-  if r == nil then return R, G, B end
-  R, G, B = r, g, b
-  print(string.format('%5.2f  %.3f %.3f %.3f', T, r, g, b))
+local t0, r0, g0, b0 = 0, 0, 0, 0
+local t1, r1, g1, b1 = 0, 0, 0, 0
+
+function love.update(dt)
+  T = T + dt
+  while T > t1 do
+    t0, r0, g0, b0 = t1, r1, g1, b1
+    local result
+    result, t1, r1, g1, b1 = coroutine.resume(co_duck)
+    if not result then
+      print('Error')
+      break
+    else
+      print(t1, r1, g1, b1)
+    end
+  end
 end
 
-local fenv = {
-  math = math,
-  os = { time = os.time },
-
-  delay = delay,
-  output = output,
-}
-local f, e = load(io.read('a') .. '\nreturn light', 'duck', 't', fenv)
-local light = f()
-light()
+function love.draw()
+  love.graphics.clear(0.98, 0.98, 0.975, 1)
+  love.graphics.setColor(r0, g0, b0)
+  local w, h = love.graphics.getDimensions()
+  local x = w / 2
+  local y = h / 2
+  local r = math.min(x, y) * 0.3
+  love.graphics.circle('fill', x, y, r)
+  love.graphics.setColor(0.1, 0.1, 0.1)
+  love.graphics.circle('line', x, y, r)
+  love.graphics.printf(string.format('%.2f', T), 0, h * 0.75, w, 'center')
+end
