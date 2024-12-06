4ccb577fd0a9db5c51190f834beea4d6d13956a2
Ayu <claire@ayu.land>
2024-12-06 23:56:29 +0800

    fw: Switch to LEDC for PWM light output

--- fw/main/led.c
+++ fw/main/led.c
@@ -2,18 +2,29 @@
 
 #include "freertos/FreeRTOS.h"
 #include "esp_log.h"
-#include "driver/gpio.h"
 
-#include "led_strip.h"
+#define USE_LED_STRIP 1
+#define USE_LED_PWM   1
 
 static const char *TAG = "LED";
 #define PIN_ONBOARD_LED GPIO_NUM_48
 // Remember to close the solder bridge
 
+#if USE_LED_STRIP
+#include "led_strip.h"
 static led_strip_handle_t led_strip;
+#endif
+
+#if USE_LED_PWM
+#include "driver/ledc.h"
+#include "driver/gpio.h"
+#endif
+
+static inline void output_tint(int r, int g, int b);
 
 void led_init()
 {
+#if USE_LED_STRIP
   /// LED strip common configuration
   led_strip_config_t strip_config = {
     .strip_gpio_num = PIN_ONBOARD_LED,  // The GPIO that connected to the LED strip's data line
@@ -39,28 +50,72 @@ void led_init()
   ESP_ERROR_CHECK(led_strip_new_rmt_device(&strip_config, &rmt_config, &led_strip));
 
   ESP_LOGI(TAG, "Initialised LED with RMT backend");
-  led_strip_set_pixel(led_strip, 0, 10, 6, 2);
-  led_strip_refresh(led_strip);
+#endif
 
-  // XXX: Testing. Write to GPIO
+#if USE_LED_PWM
+if (1) {
+  ESP_ERROR_CHECK(ledc_timer_config(&(ledc_timer_config_t){
+    .speed_mode = LEDC_LOW_SPEED_MODE,
+    .duty_resolution = LEDC_TIMER_13_BIT,
+    .timer_num = LEDC_TIMER_0,
+    .freq_hz = 4000,
+    .clk_cfg = LEDC_AUTO_CLK,
+  }));
+  static const struct { ledc_channel_t ch; int pin; } channels[3] = {
+    {LEDC_CHANNEL_0, 17},
+    {LEDC_CHANNEL_1, 18},
+    {LEDC_CHANNEL_2, 21},
+  };
+  for (int i = 0; i < 3; i++)
+    ESP_ERROR_CHECK(ledc_channel_config(&(ledc_channel_config_t){
+      .speed_mode = LEDC_LOW_SPEED_MODE,
+      .channel = channels[i].ch,
+      .timer_sel = LEDC_TIMER_0,
+      .intr_type = LEDC_INTR_DISABLE,
+      .gpio_num = channels[i].pin,
+      .duty = 0,
+      .hpoint = 0,
+    }));
+} else {
   gpio_config(&(gpio_config_t){
     .pin_bit_mask = (1 << 17) | (1 << 18) | (1 << 21),
     .mode = GPIO_MODE_OUTPUT,
     .pull_up_en = GPIO_PULLUP_ENABLE,
   });
-  gpio_set_level(17, 0);
-  gpio_set_level(18, 0);
-  gpio_set_level(21, 0);
+}
+
+  ESP_LOGI(TAG, "Initialised LED with PWM controller");
+#endif
+
+  output_tint(0, 0, 0);
+}
+
+static inline void output_tint(int r, int g, int b)
+{
+#if USE_LED_STRIP
+  led_strip_set_pixel(led_strip, 0, r, g, b);
+  led_strip_refresh(led_strip);
+#endif
+
+#if USE_LED_PWM
+if (1) {
+  ledc_set_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0, ((1 << 13) - 1) - (r << 5));
+  ledc_set_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_1, ((1 << 13) - 1) - (g << 5));
+  ledc_set_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_2, ((1 << 13) - 1) - (b << 5));
+  ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0);
+  ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_1);
+  ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_2);
+} else {
+  gpio_set_level(17, !(r >= 5));
+  gpio_set_level(18, !(g >= 5));
+  gpio_set_level(21, !(b >= 5));
+}
+#endif
 }
 
 void led_set_state(int state)
 {
-  if (state == 1) led_strip_set_pixel(led_strip, 0, 2, 6, 10);
-  if (state == 2) led_strip_set_pixel(led_strip, 0, 2, 2, 2);
-  if (state == 3) led_strip_set_pixel(led_strip, 0, 10, 6, 2);
-  led_strip_refresh(led_strip);
-
-  gpio_set_level(17, state == 1 ? 0 : 1);
-  gpio_set_level(18, state == 2 ? 0 : 1);
-  gpio_set_level(21, state == 3 ? 0 : 1);
+  if (state == 1) output_tint(2, 6, 10);
+  if (state == 2) output_tint(2, 2, 2);
+  if (state == 3) output_tint(10, 6, 2);
 }
