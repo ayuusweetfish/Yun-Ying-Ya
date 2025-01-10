bd81da4583ea850b03b39e0a39bb04f28c20cfbf
Ayu <claire@ayu.land>
2025-01-10 17:46:47 +0800

    misc: Complete Opus decoding subroutines

--- misc/opus_decode/1.js
+++ misc/opus_decode/1.js
@@ -8,5 +8,20 @@ const wasmInstance = new WebAssembly.Instance(wasmModule, {
     proc_exit: () => console.log('proc_exit'),
   },
 })
-const decoder = wasmInstance.exports.create()
-console.log(decoder)
+const initErr = wasmInstance.exports.init()
+if (initErr !== 0) {
+  throw new Error(`Error initialising instance: code ${initErr}`)
+}
+const packet = [
+  0x48, 0x0b, 0xe4, 0xe0, 0xfa, 0xe3, 0xdc, 0xbe,
+  0x35, 0x44, 0xb0, 0xb7, 0xec, 0x79, 0x3a, 0x80,
+]
+const inBufAddr = wasmInstance.exports.in_buffer()
+const outBufAddr = wasmInstance.exports.out_buffer()
+;(new Uint8Array(wasmInstance.exports.memory.buffer, inBufAddr)).set(packet)
+const n = wasmInstance.exports.decode(packet.length)
+if (n < 0) {
+  throw new Error(`Error decoding: code ${n}`)
+}
+const outBuf = new Int16Array(wasmInstance.exports.memory.buffer, outBufAddr, n)
+console.log(outBuf)
--- misc/opus_decode/duck_opus_decoder.c
+++ misc/opus_decode/duck_opus_decoder.c
@@ -3,32 +3,31 @@
 #include <stdint.h>
 #include <emscripten.h>
 
-EMSCRIPTEN_KEEPALIVE
-void *create()
-{
-  int err;
-  OpusDecoder *decoder = opus_decoder_create(16000, 1, &err);
-  if (err != OPUS_OK) return (void *)err;
-  return decoder;
-}
+static OpusDecoder *decoder;
 
 EMSCRIPTEN_KEEPALIVE
-void destroy(void *_d)
+int init()
 {
-  OpusDecoder *decoder = _d;
-  opus_decoder_destroy(decoder);
+  int err;
+  decoder = opus_decoder_create(16000, 1, &err);
+  return err;
 }
+
+static uint8_t _in_buffer[4096];
+static int16_t _out_buffer[960];
+
+EMSCRIPTEN_KEEPALIVE void *in_buffer() { return _in_buffer; }
+EMSCRIPTEN_KEEPALIVE void *out_buffer() { return _out_buffer; }
 
 // Decode a packet
 // `in_size`: number of bytes
 // `out_size`: number of samples (int16's)
 EMSCRIPTEN_KEEPALIVE
-int decode(void *_d, const uint8_t *in, int in_size, int16_t *out, int out_size)
+int decode(int in_size)
 {
-  OpusDecoder *decoder = _d;
   int n_decoded = opus_decode(
-    decoder, in, in_size,
-    out, out_size,
+    decoder, _in_buffer, in_size,
+    _out_buffer, sizeof _out_buffer / sizeof(int16_t),
     0 /* in-band forward error correction */);
   return n_decoded;
 }
