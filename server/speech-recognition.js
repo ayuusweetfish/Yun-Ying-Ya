e5b4b89e9c5b379a817bfe7c6ad099315858a961
Ayu <claire@ayu.land>
2025-12-27 21:25:22 +0800

    server: Migrate to Aliyun Gummy ASR (WIP)

--- server/speech-recognition.js
+++ server/speech-recognition.js
@@ -1,58 +1,44 @@
-const APP_ID = Deno.env.get('API_KEY_IFLYTEK_APP_ID') || prompt('App ID (iFlytek):')
-const API_KEY = Deno.env.get('API_KEY_IFLYTEK_KEY') || prompt('API key (iFlytek):')
-const API_SECRET = Deno.env.get('API_KEY_IFLYTEK_SECRET') || prompt('API secret (iFlytek):')
+const API_KEY = Deno.env.get('API_KEY_ALIYUN') || prompt('API key (Aliyun Bailian):')
 
-import { encodeBase64 } from 'jsr:@std/encoding/base64'
-import { Buffer } from 'node:buffer'
-
-const rfc1123 = (d) => d.toLocaleString('en-GB', {
-  timeZone: 'UTC',
-  hour12: false,
-  weekday: 'short',
-  year: 'numeric',
-  month: 'short',
-  day: '2-digit',
-  hour: '2-digit',
-  minute: '2-digit',
-  second: '2-digit',
-}).replace(/(?<=\d),/, '') + ' GMT'
-
-const { createHmac } = await import('node:crypto')
-
-// Input: plain text and key
-// Output: Base64 encoded
-const base64_HMAC_SHA256 = (v, k) => createHmac('sha256', k).update(v).digest('base64')
+import crypto from 'node:crypto'
 
 export const speechRecognition = () => new Promise((resolve, reject) => {
-  const host = 'iat-api.xfyun.cn'
-  const date = rfc1123(new Date())
-  const requestLine = `GET /v2/iat HTTP/1.1`
-  const signedString = `host: ${host}\ndate: ${date}\n${requestLine}`
-  const signature = base64_HMAC_SHA256(signedString, API_SECRET)
-
-  const authString = `api_key="${API_KEY}", algorithm="hmac-sha256", headers="host date request-line", signature="${signature}"`
-  const auth = btoa(authString)
-
-  const url =
-    `wss://iat-api.xfyun.cn/v2/iat?authorization=${encodeURIComponent(auth)}` +
-    `&date=${encodeURIComponent(date)}&host=${host}`
+  const ws = new WebSocket(
+    'wss://dashscope.aliyuncs.com/api-ws/v1/inference', {
+      headers: {
+        'Authorization': `Bearer ${API_KEY}`,
+      },
+    })
 
   let ret
-  let promiseFinished = false   // For the current call (creation)
-
-  let recognitionResult = []
-
-  let endPromiseResolve = null  // For the `end()` call
-  let endPromiseTimer = null
+  let creationPromiseDone = false
+  let creationTimeoutTimer
 
   let expectedClose = false
-  let remoteEarlyFinish = false
 
-  const ws = new WebSocket(url)
   ws.onopen = (ev) => {
     console.log('open')
-    resolve(ret)
-    promiseFinished = true
+    ws.send(JSON.stringify({
+      header: {
+        action: 'run-task',
+        task_id: crypto.randomUUID(),
+        streaming: 'duplex',
+      },
+      payload: {
+        task_group: 'audio',
+        task: 'asr',
+        function: 'recognition',
+        model: 'gummy-chat-v1',
+        input: {},
+        parameters: {
+          sample_rate: 16000,
+          format: 'pcm',
+          transcription_enabled: true,
+          translation_enabled: false,
+          max_end_silence: 1000,
+        },
+      },
+    }))
   }
   ws.onclose = (ev) => {
     console.log('close', ev.code, ev.reason, ev.wasClean)
@@ -60,9 +46,10 @@ export const speechRecognition = () => new Promise((resolve, reject) => {
   }
   ws.onerror = (ev) => {
     console.log('error', ev.message)
-    if (!promiseFinished) {
+    if (!creationPromiseDone) {
       reject(new Error(ev.message))
-      promiseFinished = true
+      creationPromiseDone = true
+      clearTimeout(creationTimeoutTimer)
     }
     expectedClose = true
     ws.close()
@@ -70,20 +57,11 @@ export const speechRecognition = () => new Promise((resolve, reject) => {
   ws.onmessage = (ev) => {
     try {
       const o = JSON.parse(ev.data)
-      // console.log('data', Deno.inspect(o, { depth: 99 }))
-      const sentence =
-        o.data.result.ws.map((wsEntry) =>
-          wsEntry.cw.map((cwEntry) => cwEntry.w).join('')).join('')
-      recognitionResult.push(sentence)
-      if (o.data.status === 2) {
-        if (endPromiseResolve) {
-          clearTimeout(endPromiseTimer)
-          endPromiseResolve(recognitionResult.join(''))
-        } else {
-          remoteEarlyFinish = true
-        }
-        expectedClose = true
-        ws.close()
+      console.log('data', Deno.inspect(o, { depth: 99 }))
+      if (o.header && o.header.event === 'task-started' && !creationPromiseDone) {
+        resolve(ret)
+        creationPromiseDone = true
+        clearTimeout(creationTimeoutTimer)
       }
     } catch (e) {
       console.log(e.message)
@@ -91,87 +69,14 @@ export const speechRecognition = () => new Promise((resolve, reject) => {
     }
   }
 
-  setTimeout(() => {
-    if (!promiseFinished) {
-      reject(new Error('Cannot connect to speech recognition server'))
-      promiseFinished = true
-      expectedClose = true
-      ws.close()
-    }
-  }, 5000)
+  creationTimeoutTimer = setTimeout(
+    () => ws.onerror({ message: 'Start-up imeout' }), 5000)
 
-  let first = true
-  const sendAudio = (pcm, last) => {
-    if (pcm.length === 0 && !last) { return }
-    if (remoteEarlyFinish) { return }
-    const o = {
-      data: {
-        status: 1,
-        format: 'audio/L16;rate=16000',
-        encoding: 'raw',
-        audio: encodeBase64(pcm),
-      }
-    }
-    if (first) {
-      first = false
-      Object.assign(o, {
-        common: { app_id: APP_ID },
-        business: {
-          language: 'zh_cn',
-          domain: 'iat',
-          accent: 'mandarin',
-        },
-      })
-      o.data.status = 0
-    }
-    if (last) {
-      o.data.status = 2
-    }
-    console.log('send audio', pcm.length, !!last, Date.now(), o.data.status)
-    ws.send(JSON.stringify(o))
-  }
-
-  const FRAME = 1280
-  let residue = Buffer.alloc(0)
-  let lastSend = new Date(0)
   const push = (pcm) => {
-    const buf = Buffer.concat([residue, pcm])
-    if (new Date() - lastSend < 40) {
-      residue = buf
-    } else {
-      const sendLen = buf.length - buf.length % FRAME
-      residue = Buffer.alloc(buf.length % FRAME)
-      // residue[:] = buf[sendLen:]
-      buf.copy(residue, 0, sendLen)
-      sendAudio(buf.slice(0, sendLen))
-      lastSend = new Date()
-    }
   }
 
   const end = () => new Promise((resolve, reject) => {
-    if (remoteEarlyFinish) {
-      resolve(recognitionResult.join(''))
-      return
-    }
-    endPromiseResolve = resolve
-    endPromiseTimer = setTimeout(() => {
-      if (endPromiseTimer !== null) {
-        endPromiseResolve = null
-        expectedClose = true
-        ws.close()
-        reject(new Error('Server did not return valid recognition results in time'))
-      }
-    }, 5000)
-    if (new Date() - lastSend < 40) {
-      setTimeout(() => {
-        // Workaround: server may not respond timely if sent a large chunk marked finish?
-        sendAudio(residue, false)
-        sendAudio(Buffer.alloc(0), true)
-      }, Math.max(10, 40 - (new Date() - lastSend)))
-    } else {
-      sendAudio(residue, false)
-      sendAudio(Buffer.alloc(0), true)
-    }
+    resolve('TODO.')
   })
 
   ret = {
@@ -179,13 +84,3 @@ export const speechRecognition = () => new Promise((resolve, reject) => {
     end,
   }
 })
-
-// ======== Test run ======== //
-if (import.meta.main) {
-  // ffmpeg -i 6-聆小璐-助理.wav -t 4 -ar 16000 -f s16le -acodec pcm_s16le 聆小璐.pcm
-  const r = await speechRecognition()
-  const fileContent = await Deno.readFile('聆小璐.pcm')
-  r.push(fileContent)
-  const s = await r.end()
-  console.log(s)
-}
